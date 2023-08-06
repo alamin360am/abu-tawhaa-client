3898c92523a52588b047786e3549ec24292dfe99
Al - Amin <alaminhimu0@gmail.com>
2023-08-07 01:04:07 +0600

    bangladesh api created

--- src/pages/MahofilForm/MahofilForm.jsx
+++ src/pages/MahofilForm/MahofilForm.jsx
@@ -1,10 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import Heading from "../../components/Heading";
 import Quote from "../../components/Quote";
 import inputImg from "./../../../public/formImg.png";
 
 const MahofilForm = () => {
+  const [divisions, setDivisions] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [upazelas, setUpazela] = useState([]);
+  const [unions, setUnions] = useState([]);
+  const [selectedDivision, setSelectedDivision] = useState([]);
+  const [selectedDistrict, setSelectedDistrict] = useState([]);
+  const [selectedUpazela, setSelectedUpazela] = useState([]);
+  const [selectedUnion, setSelectedUnion] = useState([]);
+  const [filteredDistricts, setFilteredDistricts] = useState([]);
+  const [filteredUpazelas, setFilteredUpazelas] = useState([]);
+  const [filteredUnions, setFilteredUnions] = useState([]);
+
+  useEffect(() => {
+    fetch("/Bangladesh/division.json")
+      .then((res) => res.json())
+      .then((data) => setDivisions(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("/Bangladesh/district.json")
+      .then((res) => res.json())
+      .then((data) => setDistricts(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("/Bangladesh/upazela.json")
+      .then((res) => res.json())
+      .then((data) => setUpazela(data));
+  }, []);
+
+  useEffect(() => {
+    fetch("/Bangladesh/union.json")
+      .then((res) => res.json())
+      .then((data) => setUnions(data));
+  }, []);
+
+  const divisionSelect = (event) => {
+    setSelectedDivision(event.target.value);
+    const districtOfDivision = districts.filter(
+      (district) => district.division_name == event.target.value
+    );
+    setFilteredDistricts(districtOfDivision);
+  };
+
+  const districtSelect = (event) => {
+    setSelectedUpazela(event.target.value);
+    const upazelaOfDistrict = upazelas.filter(
+      (upazela) => upazela.district_name == event.target.value
+    );
+    setFilteredUpazelas(upazelaOfDistrict);
+  };
+
+  const upazelaSelect = (event) => {
+    setSelectedDistrict(event.target.value);
+    const unionOfUpazela = unions.filter(
+      (union) => union.upazilla_name == event.target.value
+    );
+    setFilteredUnions(unionOfUpazela);
+  };
+
+  const unionSelect = (event) => {
+    setSelectedUnion(event.target.value);
+  };
+
+  console.log(selectedDistrict);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const applicantName = form.applicantName.value;
+    const mobileNo = form.mobileNo.value;
+    const date = form.date.value;
+    const applicationData = {
+      applicantName,
+      mobileNo,
+      date,
+      selectedDivision,
+      selectedDistrict,
+      selectedUpazela,
+      selectedUnion,
+    };
+    console.log(applicationData);
+  };
+
   return (
     <section className="md:p-8 p-4 bg-green-200">
       <Helmet>
@@ -20,7 +104,10 @@ const MahofilForm = () => {
         ফর্মের নিচে আমাদের নীতিমালা ও শর্তাবলী দেওয়া আছে। আবেদনের পূর্বে
         নীতিমালা ও শর্তাবলী পড়ে নিবেন ইনশাআল্লাহ।
       </p>
-      <form className="flex items-center flex-col p-4 gap-4">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center flex-col p-4 gap-4"
+      >
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
             আবেদনকারীর নাম
@@ -28,8 +115,7 @@ const MahofilForm = () => {
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
             type="text"
-            name=""
-            id=""
+            name="applicantName"
             className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
           />
         </div>
@@ -40,14 +126,103 @@ const MahofilForm = () => {
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
             type="number"
-            name=""
+            name="mobileNo"
             id=""
             className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
           />
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিলের প্রস্তাবিত তারিখ
+            মাহফিলের প্রস্তাবিত তারিখ
+          </p>
+          <img src={inputImg} alt="" className="h-full z-10" />
+          <input
+            type="date"
+            name="date"
+            id=""
+            className="w-full pl-16 pr-4 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
+          />
+        </div>
+        <div className="md:w-[600px] w-full gap-2 flex flex-col md:flex-row md:gap-10 items-center">
+          <div className="w-full h-full flex items-center relative">
+            <p className="absolute -top-2 left-20 text-green-700">
+              মাহফিল কোন বিভাগে করতে চান?
+            </p>
+            <img src={inputImg} className="h-20 z-10" />
+            <select
+              value={selectedDivision}
+              onChange={divisionSelect}
+              className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
+            >
+              <option value="">বিভাগ নির্বাচন করুন</option>
+              {divisions.map((division) => (
+                <option key={division.id} value={division.bn_name}>
+                  {division.bn_name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-full h-full flex items-center relative">
+            <p className="absolute -top-5 left-20 text-green-700">
+              মাহফিল কোন জেলায় করতে চান?
+            </p>
+            <img src={inputImg} alt="" className="h-20 z-10" />
+            <select
+              onChange={districtSelect}
+              className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
+            >
+              <option value="">জেলা নির্বাচন করুন</option>
+              {filteredDistricts.map((district) => (
+                <option key={district?.id} value={district?.bn_name}>
+                  {district?.bn_name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="md:w-[600px] w-full gap-2 flex flex-col md:flex-row md:gap-10 items-center">
+          <div className="w-full h-full flex items-center relative">
+            <p className="absolute -top-5 left-20 text-green-700">
+              মাহফিল কোন উপজেলা করতে চান?
+            </p>
+            <img src={inputImg} alt="" className="h-20 z-10" />
+            <select
+              name=""
+              id=""
+              onChange={upazelaSelect}
+              className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
+            >
+              <option value="">উপজেলা নির্বাচন করুন</option>
+              {filteredUpazelas.map((upazela) => (
+                <option key={upazela?.id} value={upazela?.bn_name}>
+                  {upazela?.bn_name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="w-full h-full flex items-center relative">
+            <p className="absolute -top-5 left-20 text-green-700">
+              মাহফিল কোন ইউনিয়নে করতে চান?
+            </p>
+            <img src={inputImg} alt="" className="h-20 z-10" />
+            <select
+              name=""
+              id=""
+              onChange={unionSelect}
+              className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
+            >
+              <option value="">ইউনিয়ন নির্বাচন করুন</option>
+              {filteredUnions.map((union) => (
+                <option key={union?.id} value={union?.bn_name}>
+                  {union?.bn_name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="md:w-[600px] w-full h-20 flex items-center relative">
+          <p className="absolute -top-2 left-20 text-green-700">
+            প্রোগ্রাস্থলের বিস্তারিত ঠিকানা
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -59,7 +234,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিল কোন জেলায় করতে চান?
+            গুগল ম্যাপ এর প্রোগ্রাস্থলের ঠিকানার লিংক (যদি পারেন)
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -71,7 +246,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          প্রোগ্রাস্থলের বিস্তারিত ঠিকানা
+            যে মাঠে বা মিলনায়তনে মাহফিল করতে চান তার নাম
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -83,7 +258,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          গুগল ম্যাপ এর প্রোগ্রাস্থলের ঠিকানার লিংক (যদি পারেন)
+            মাহফিলের প্রধান আয়োজক/সভাপতির নাম ও মোবাইল নম্বর
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -95,7 +270,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          যে মাঠে বা মিলনায়তনে মাহফিল করতে চান তার নাম
+            এটি এই জায়গার কততম মাহফিল?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -107,7 +282,19 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিলের প্রধান আয়োজক/সভাপতির নাম ও মোবাইল নম্বর
+            সর্বশেষ কবে এই জায়গায় মাহফিল হয়েছিল?
+          </p>
+          <img src={inputImg} alt="" className="h-full z-10" />
+          <input
+            type="date"
+            name=""
+            id=""
+            className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
+          />
+        </div>
+        <div className="md:w-[600px] w-full h-20 flex items-center relative">
+          <p className="absolute -top-2 left-20 text-green-700">
+            মাহফিলের আলোচক হিসাবে আর কে কে উপস্থিত থাকবেন?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -119,7 +306,8 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          এটি এই জায়গার কততম মাহফিল?
+            আবু ত্বহা মুহাম্মাদ আদনান কে কোন সময় থেকে কোন সময় পর্যন্ত আলোচনা
+            করতে হবে?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -131,7 +319,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          সর্বশেষ কবে এই জায়গায় মাহফিল হয়েছিল?
+            কী বিষয়ে আলোচনা করার অনুরোধ করবেন?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -143,7 +331,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিলের আলোচক হিসাবে আর কে কে উপস্থিত থাকবেন?
+            মাহফিলে আবু ত্বহা মুহাম্মাদ আদনানকে অর্থ কালেকশন করতে হবে কি না?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -155,7 +343,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          আবু ত্বহা মুহাম্মাদ আদনান কে কোন সময় থেকে কোন সময় পর্যন্ত আলোচনা করতে হবে?
+            বিগত বছরের মাহফিলে আলোচক ছিলেন কে কে?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -167,7 +355,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          কী বিষয়ে আলোচনা করার অনুরোধ করবেন?
+            বিগত বছরের মাহফিলের জন্য প্রশাসনের অনুমতি নেওয়া হয়েছিল কি না?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -179,7 +367,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিলে আবু ত্বহা মুহাম্মাদ আদনানকে অর্থ কালেকশন করতে হবে কি না?
+            এই বছরের মাহফিলের জন্য প্রশাসনের লিখিত অনুমতি নিতে পারবেন কিনা?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -191,7 +379,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          বিগত বছরের মাহফিলে আলোচক ছিলেন কে কে?
+            পর্যাপ্ত ভলান্টিয়ার থাকবে কি না ?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -203,7 +391,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          বিগত বছরের মাহফিলের জন্য প্রশাসনের অনুমতি নেওয়া হয়েছিল কি না?
+            মাহফিলের সাথে কোনো ধর্মীয় দলের সম্পৃক্ততা আছে কি না?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -215,7 +403,8 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          এই বছরের মাহফিলের জন্য প্রশাসনের লিখিত অনুমতি নিতে পারবেন কিনা?
+            মাহফিলটি কোন ঘোরানার মানুষদের মধ্যে হবে? ( হানাফি/ আহলে হাদিস/
+            তাবলীগ/ জামায়াত/ চরমোনাই ইত্যাদি )
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -227,7 +416,8 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          পর্যাপ্ত ভলান্টিয়ার থাকবে কি না ?
+            মাহফিলে কোনো রাজনৈতিক ব্যক্তি উপস্থিত থাকবেন কিনা? ( থাকলে তার নাম ও
+            রাজনৈতিক পদবী )
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -239,7 +429,7 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিলের সাথে কোনো ধর্মীয় দলের সম্পৃক্ততা আছে কি না?
+            আবু ত্বহা আদনান এর জন্য সম্ভাব্য পাথেয় খরচ কত?
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -251,7 +441,9 @@ const MahofilForm = () => {
         </div>
         <div className="md:w-[600px] w-full h-20 flex items-center relative">
           <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিলটি কোন ঘোরানার মানুষদের মধ্যে হবে? ( হানাফি/ আহলে হাদিস/ তাবলীগ/ জামায়াত/ চরমোনাই ইত্যাদি )
+            বক্তার ব্যক্তিগত, অসুস্থতা, প্রশাসনিক ও অন্যান্য প্রাসঙ্গিক কারণে
+            বক্তা কর্তৃক মাহফিল স্থগিত কিংবা তারিখ পরিবর্তন করার সম্পুর্ন
+            এখতিয়ার থাকবে
           </p>
           <img src={inputImg} alt="" className="h-full z-10" />
           <input
@@ -261,42 +453,11 @@ const MahofilForm = () => {
             className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
           />
         </div>
-        <div className="md:w-[600px] w-full h-20 flex items-center relative">
-          <p className="absolute -top-2 left-20 text-green-700">
-          মাহফিলে কোনো রাজনৈতিক ব্যক্তি উপস্থিত থাকবেন কিনা? ( থাকলে তার নাম ও রাজনৈতিক পদবী )
-          </p>
-          <img src={inputImg} alt="" className="h-full z-10" />
-          <input
-            type="text"
-            name=""
-            id=""
-            className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
-          />
-        </div>
-        <div className="md:w-[600px] w-full h-20 flex items-center relative">
-          <p className="absolute -top-2 left-20 text-green-700">
-          আবু ত্বহা আদনান এর জন্য সম্ভাব্য পাথেয় খরচ কত?
-          </p>
-          <img src={inputImg} alt="" className="h-full z-10" />
-          <input
-            type="text"
-            name=""
-            id=""
-            className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
-          />
-        </div>
-        <div className="md:w-[600px] w-full h-20 flex items-center relative">
-          <p className="absolute -top-2 left-20 text-green-700">
-          বক্তার ব্যক্তিগত, অসুস্থতা, প্রশাসনিক ও অন্যান্য প্রাসঙ্গিক কারণে বক্তা কর্তৃক মাহফিল স্থগিত কিংবা তারিখ পরিবর্তন করার সম্পুর্ন এখতিয়ার থাকবে
-          </p>
-          <img src={inputImg} alt="" className="h-full z-10" />
-          <input
-            type="text"
-            name=""
-            id=""
-            className="w-full pl-16 pr-2 py-2 absolute z-0 left-6 rounded-full bg-white focus:outline-none border-4 border-green-800"
-          />
-        </div>
+        <input
+          type="submit"
+          value="আবেদন করুন"
+          className="bg-green-700 text-white px-8 py-4"
+        />
       </form>
     </section>
   );
